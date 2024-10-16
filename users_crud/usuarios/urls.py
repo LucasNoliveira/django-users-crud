9d2a0edba5f266ea0dad07a34e62327c03566716
Lucas Oliveira <lucasnoliveira126@gmail.com>
2024-10-16 14:01:40 -0300

    boas práticas de design de api, versionamento de api v1

--- users_crud/usuarios/urls.py
+++ users_crud/usuarios/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, LoginView
 
 router = DefaultRouter()
-router.register('usuarios', UserViewSet)
+router.register('users', UserViewSet)
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
